263013ceb1535be963ec05267240b0358e3f4c2c
Subhajit Basak <76099931+heresaheb@users.noreply.github.com>
2022-04-05 08:04:51 -0700

    Add files via upload

---
+++ NextPermutation.java
@@ -0,0 +1,42 @@
+public class NextPermutation {
+
+    // (Question):  A permutation of integers is an
+    //  arrangement of its members into a sequence or linear order.
+
+    public static int[] permutationArray(int a[]) {
+
+        for (int i = a.length - 1; i > 0; i--) {
+            if (a[i] > a[i - 1]) {
+                swap(a, i, i - 1); // 1 2 3 1 3 2 2 1 3 2 3 1
+                return a;
+            }
+        }
+
+        return a;
+
+    }
+
+    public static int[] swap(int[] arr, int i, int j) {
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3 };
+        permutationArray(arr);
+
+        System.out.println("Next Permutation Will be Here:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+}
+
+// Output:
+// Next Permutation Will be Here:
+// 1 3 2
