81e0489df00b7dc84ee59f5ff07fdd0413f11e1f
Subhajit Basak <76099931+heresaheb@users.noreply.github.com>
2022-05-01 12:54:31 +0530

    Create occurance.java
    First and Last Occurances - By using Arraylist & a 1D Array.
    Qs. Link:
     https://www.codingninjas.com/codestudio/problems/first-and-last-position-of-an-element-in-sorted-array_1082549?leftPanelTab=2

---
+++ occurance.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class occurance {
+
+    public static int[] firstAndLastPosition(ArrayList<Integer> arr, int n, int k) {
+        // Write your code here.
+		int count =1;
+		int a [] = new int[2]; 
+		a[0] =-1;
+		a[1]=-1;
+		int i=0;
+		for(int temp : arr){
+			
+			if(temp == k){
+				
+				if(i==0){
+				a[i]= count-1;   //1st occur
+				i++;
+				}
+				
+				 if(i==1){
+					a[i]= count-1;  //last occur
+				 }
+				
+			}
+			
+			else if(a[0]!= -1 && a[1] == -1 && count == n){
+				a[1] = a[0];
+			} 
+			
+		    else if(a[0]== -1 && count == n){
+				a[0]= -1;
+				a[1]= -1;
+			}
+			count++;
+		}
+		
+		return a;
+		
+    }
+
+};
